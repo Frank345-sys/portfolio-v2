3168fbc1e14066e705f817ac25ef1053ebb59bea
Frank345-sys <francgonzalez456@gmail.com>
2026-04-13 18:32:44 -0600

    refactor(components): actualizar barrel constants

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -1,2 +1,10 @@
+/**
+ * Constantes compartidas: tokens de diseño, enums, etiquetas de skills y presets Motion.
+ *
+ * @module shared/constants
+ */
 export * from './tokens'
 export * from './enums'
+export * from './skills'
+export * from './motion'
+export * from './breakpoints'
