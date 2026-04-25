a987d018df8af988877cc3e595cff963b251561a
Frank345-sys <francgonzalez456@gmail.com>
2026-04-24 19:20:26 -0600

    feat(projects): ajustar index.ts

--- src/components/ProjectsSection/hooks/index.ts
+++ src/components/ProjectsSection/hooks/index.ts
@@ -4,11 +4,4 @@
  * @module components/ProjectsSection/hooks
  */
 export { useProjectPreviewCard } from './useProjectPreviewCard'
-export type {
-  UseProjectPreviewCardParams,
-  UseProjectPreviewCardResult,
-} from './useProjectPreviewCard'
-export { useProjectsScrollSync } from './useProjectsScrollSync'
-export type { UseProjectsScrollSyncResult } from './useProjectsScrollSync'
 export { useProjectsSection } from './useProjectsSection'
-export type { UseProjectsSectionResult } from './useProjectsSection'
